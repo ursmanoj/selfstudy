3bf65d14ab384a01ef41d59fb692575b2c8f64e2
Manoj Sharma <ursmanoj@gmail.com>
2017-08-20 16:52:47 +0900

    JSRDbg GUI in C# sample added

---
+++ spidermonkey/JSRdbg/WinFormDebugger/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxResponse = new System.Windows.Forms.TextBox();
+            this.srcCodeList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxResponse
+            // 
+            this.txtBoxResponse.Location = new System.Drawing.Point(744, 24);
+            this.txtBoxResponse.Multiline = true;
+            this.txtBoxResponse.Name = "txtBoxResponse";
+            this.txtBoxResponse.ReadOnly = true;
+            this.txtBoxResponse.Size = new System.Drawing.Size(705, 834);
+            this.txtBoxResponse.TabIndex = 0;
+            // 
+            // srcCodeList
+            // 
+            this.srcCodeList.FormattingEnabled = true;
+            this.srcCodeList.ItemHeight = 16;
+            this.srcCodeList.Items.AddRange(new object[] {
+            "debugger;",
+            "print(\'Manoj. \');",
+            "var a = \"Unchanged\";",
+            "//var func = function () {",
+            "\tprint(\"Changing value of a to changed\");",
+            "\ta = \"changed\";",
+            "//}",
+            "//func();",
+            "print(\"New value of a:\" + a);"});
+            this.srcCodeList.Location = new System.Drawing.Point(141, 24);
+            this.srcCodeList.Name = "srcCodeList";
+            this.srcCodeList.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.srcCodeList.Size = new System.Drawing.Size(529, 596);
+            this.srcCodeList.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1471, 870);
+            this.Controls.Add(this.srcCodeList);
+            this.Controls.Add(this.txtBoxResponse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxResponse;
+        private System.Windows.Forms.ListBox srcCodeList;
+    }
+}
+
---
+++ spidermonkey/JSRdbg/WinFormDebugger/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            connectJSRDB();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private async void connectJSRDB()
+        {
+            //192.168.123.106 : 8089
+            string host = "192.168.123.106";
+            string port = "8089";
+            IPAddress ip = IPAddress.Parse(host);
+
+            Socket s = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            Console.WriteLine("Establishing Connection to {0}",
+                host);
+            s.Connect(ip, 8089);
+            Console.WriteLine("Connection established");
+
+            byte[] buffer = new byte[1000];
+
+            s.Receive(buffer);
+
+            txtBoxResponse.Text += "\n";
+            txtBoxResponse.Text += System.Text.Encoding.ASCII.GetString(buffer);
+
+            //JsonConvert;
+
+        }
+    }
+}
